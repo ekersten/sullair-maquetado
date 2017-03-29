42bfed828bb12606964456c4f80f58a2ce724325
Eric Kersten <eric.kersten@egoargentina.com>
2017-03-29 18:28:53 -0300

    new modules

--- js/nav.js
+++ js/nav.js
@@ -6,12 +6,12 @@
 	$navLinks.on('click', function(e) {
 		var $el = $(e.currentTarget);
 
-		$('.sub-nav').hide();
+		$('.sub-nav').not('#' + $el.data('subnav-toggle')).fadeOut(200);
 
 		if($el.data('subnav-toggle')) {
 			e.preventDefault();
 			
-			$('#' + $el.data('subnav-toggle')).show();
+			$('#' + $el.data('subnav-toggle')).fadeToggle(200);
 		}
 	});
 })(window, jQuery);
