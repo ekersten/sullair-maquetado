7a7cc8eff7ba888cb2bb6bd4f50d132405196492
Eric Kersten <eric.kersten@egoargentina.com>
2017-04-07 16:57:38 -0300

    navegacion

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,33 @@
 (function(window, $) {
-	var $navContainer = $('.nav-container'),
-		$navLinks = $navContainer.find('.navigation a');
+
+	var $navContainer = $('header nav'),
+		$navLinks = $navContainer.find('a'),
+		$overlay = $('.overlay'),
+		$subnavs = $('.subnav');
+
+	$subnavs.each(function(index, item) {
+		$sub = $(item);
+		$sub.css('top', $sub.height() * -1);
+	});
+
+	$overlay.fadeOut(0);
 
 
 	$navLinks.on('click', function(e) {
 		var $el = $(e.currentTarget);
 
-		$('.sub-nav').not('#' + $el.data('subnav-toggle')).fadeOut(200);
-
 		if($el.data('subnav-toggle')) {
 			e.preventDefault();
-			
-			$('#' + $el.data('subnav-toggle')).fadeToggle(200);
+
+			$el.toggleClass('active');
+
+			if($el.hasClass('active')) {
+				$overlay.fadeIn();
+				$('#sub-' + $el.data('subnav-toggle')).css('top', '83px');
+			} else {
+				$overlay.fadeOut();
+				$('#sub-' + $el.data('subnav-toggle')).css('top', $('#sub-' + $el.data('subnav-toggle')).height() * -1);
+			}
 		}
 	});
 })(window, jQuery);
