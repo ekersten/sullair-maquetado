8228752fafa5377442b919c50b7901f9f25e0192
Eric Kersten <eric.kersten@egoargentina.com>
2017-04-12 12:45:11 -0300

    descanso 3

---
+++ descanso3.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/sullair.css">
+	<title>Sullair</title>
+</head>
+<body class="">
+	
+	<?php include('includes/header.php'); ?>
+
+	<div class="wrapper">
+		<div class="component w8 h4 full-md">
+			<div class="slider">
+				<!-- SLIDE START -->
+				<div class="slide">
+					<div class="titulo">poder<br><span class="green">construir</span></div>
+					<div class="imagen">
+						<img src="img/excavadora.png" alt="">
+					</div>
+					<div class="copy">
+						Movimientos<br>de Tierra
+					</div>
+
+					<div class="options">
+						<div class="legend">Seleccione por <strong>altura de la plataforma [pies[m]]</strong></div>
+						<select name="" id="">
+							<option value="">0 a 30</option>
+							<option value="">30 a 60</option>
+							<option value="">mas de 60</option>
+						</select>
+					</div>
+
+					<a href="#" class="cta">Me interesa saber mas...</a>
+				</div>
+				<!-- SLIDE END -->
+			</div>
+		</div>
+	</div>
+
+	<div class="wrapper">
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="component full-width-text-right">
+		<div class="image" style="background-image: url(img/maquina1.jpg);"></div>
+		<div class="text-wrapper">
+			<div class="text">
+				<h6 class="subtitle">Subtitle</h6>
+				<h4 class="title">Mayor exigencia,<br>condiciones extremas.<br>Camiones articualdos.</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>	
+		</div>
+	</div>
+
+	<div class="wrapper">
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+		<div class="component w4 h3">
+			<div class="image">
+				<img src="img/mod_4x2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h4 class="title">Main Title</h4>
+				<a href="#" class="cta">Me interesa saber mas...</a>
+			</div>
+		</div>
+	</div>
+
+	
+	<?php include('includes/footer.php'); ?>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+	<script src="js/nav.js"></script>
+	<script src="js/table.js"></script>
+
+</body>
+</html>
